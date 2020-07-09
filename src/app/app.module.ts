2f16282a1b345f7d51fde468932143ad137ab22b
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-09 17:26:44 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PhotoCreateComponent} from './photo/photo-create/photo-create.component';
+import {PhotoDeleteComponent} from './photo/photo-delete/photo-delete.component';
+import {PhotoDetailComponent} from './photo/photo-detail/photo-detail.component';
+import {PhotoEditComponent} from './photo/photo-edit/photo-edit.component';
+import {PhotoListComponent} from './photo/photo-list/photo-list.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoCreateComponent,
+    PhotoDeleteComponent,
+    PhotoDetailComponent,
+    PhotoEditComponent,
+    PhotoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
